26f24412a631bb17e62ac332dd8e297f3f89e85d
Kevin T Boyle <kevintboyle@Kevins-MacBook-Pro.local>
2020-02-01 10:16:03 -0500

    Signed-off-by: Kevin T Boyle <kevintboyle@Kevins-MacBook-Pro.local>

---
+++ elemental_container/src/test/java/com/wallstft/AppTest.java
@@ -0,0 +1,24 @@
+package com.wallstft;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@DisplayName("Test shouldAnswerWithTrue()")
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+		System.out.println("Hello World");
+        assertTrue( true );
+    }
+}
