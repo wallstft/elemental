5782e398dcb6514cf1f4547bbab72366d6c3d18d
Kevin T Boyle <kevintboyle@Kevins-MBP-2.fios-router.home>
2020-03-31 20:12:40 -0400

    Signed-off-by: Kevin T Boyle <kevintboyle@Kevins-MBP-2.fios-router.home>

---
+++ quant/src/main/java/com/wallstft/quant/App.java
@@ -0,0 +1,17 @@
+package com.wallstft.quant;
+
+import org.quantlib.BlackSwaptionEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+		System.out.println ("Hello World");
+    	EquityOptions.calc();
+		
+    }
+}
