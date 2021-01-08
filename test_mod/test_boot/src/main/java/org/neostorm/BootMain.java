3dff760cc42a264ef8b41aacd3c859db70e88768
Kevin T Boyle <kevintboyle@Kevins-MBP-2.fios-router.home>
2021-01-07 20:43:11 -0500

    release the maven archetype to create angular and springboot
    Signed-off-by: Kevin T Boyle <kevintboyle@Kevins-MBP-2.fios-router.home>

---
+++ test_mod/test_boot/src/main/java/org/neostorm/BootMain.java
@@ -0,0 +1,38 @@
+package org.neostorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@Controller
+//@ImportResource("classpath:spring-config.xml")
+@EnableAutoConfiguration
+public class BootMain
+{
+
+    @RequestMapping(value="/start", method= RequestMethod.POST)
+    @ResponseBody
+    void start_workflow(@RequestBody String json ) {
+
+        System.out.println("Hello World");
+    }
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BootMain.class, args);
+    }
+}
